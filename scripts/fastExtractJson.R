10c8ac24972fcd2d2777578ea780301cb81116cc
Finn <tlobnow@googlemail.com>
2023-10-24 14:11:59 +0200

    added loading step for required packages

--- scripts/fastExtractJson.R
+++ scripts/fastExtractJson.R
@@ -1,5 +1,3 @@
-pacman::p_load(dplyr, tidyr, stringr, fs, jsonlite, purrr, utils, data.table)
-
 source(file = ifelse(exists("https://raw.githubusercontent.com/tlobnow/myddosome_proteomics/main/scripts/functions.R"), 
                      yes =  "https://raw.githubusercontent.com/tlobnow/myddosome_proteomics/main/scripts/functions.R",
                      no  =  "~/Documents/Github/myddosome_proteomics/scripts/functions.R"))
@@ -13,15 +11,15 @@ source(file = ifelse(exists("https://raw.githubusercontent.com/tlobnow/myddosome
 # LOC = "TEMP"
 # LOC = "BDLD"
 LOC = "ADLD"
-LOC = "EDLD"
+# LOC = "EDLD"
 
 # MAIN    = file.path("/Volumes/TAYLOR-LAB/Finn/CURATED_RESULTS", LOC, "/")
 # MAIN    = file.path("/Volumes/TAYLOR-LAB/Finn/RESULTS/IP_MS_2/", LOC, "/")
 # MAIN    = file.path("/Users/u_lobnow/Documents/Github/", LOC, "/")
 # MAIN    = file.path("/Users/u_lobnow/Documents/Github/transferGit/", LOC, "/")
-MAIN    = file.path("/Volumes/TAYLOR-LAB/Finn/CURATED_RESULTS/", LOC, "/")
+# MAIN    = file.path("/Volumes/TAYLOR-LAB/Finn/CURATED_RESULTS/", LOC, "/")
 
-run_extraction(LOC = LOC, MAIN = MAIN)
-
+run_extraction(LOC = LOC)
+plot_alphafold_results(LOC = LOC)
 
 
--- scripts/functions.R
+++ scripts/functions.R
@@ -324,7 +324,7 @@ add2Summary <- function(NEW_DF, EXISTING_DF) {
 ################################################################################
 
 run_extraction <- function(LOC, MAIN = NULL, SUMMARY_FOLDER = NULL, ADD_2_EXISTING_DF = F, EXISTING_DF = NULL) {
-  
+  pacman::p_load(dplyr, tidyr, stringr, fs, jsonlite, purrr, utils, data.table)
   if (is.null(MAIN)) {
     MAIN <- paste0("/Volumes/TAYLOR-LAB/Finn/CURATED_RESULTS/", LOC, "/")
   }
